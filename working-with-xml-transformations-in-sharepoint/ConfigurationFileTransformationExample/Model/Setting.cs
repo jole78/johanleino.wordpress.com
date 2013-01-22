5acc9e64111d33a5ee6ef977442e3ef985c8c463
Johan Leino <leino@outlook.com>
2013-01-22 22:37:20 +0100

    Moved code from codeplex

---
+++ working-with-xml-transformations-in-sharepoint/ConfigurationFileTransformationExample/Model/Setting.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConfigurationFileTransformationExample.Model
+{
+    class Setting
+    {
+        public const string Prefix = "appSetting__";
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        internal static string BuildWebSitePropertyKey(string key)
+        {
+            return string.Concat(Prefix, key);
+        }
+    }
+}
---
+++ working-with-xml-transformations-in-sharepoint/ConfigurationFileTransformationExample/Services/ISettingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConfigurationFileTransformationExample.Model;
+
+namespace ConfigurationFileTransformationExample.Services
+{
+    interface ISettingService
+    {
+        bool TryLocateByKey(string key, out Setting value);
+
+        IEnumerable<Setting> FindAll();
+    }
+}
