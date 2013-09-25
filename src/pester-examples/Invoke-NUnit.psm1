cdcd6b92f1ba1bb2396b29db157255e24b9f2e4e
Johan Leino <leino@outlook.com>
2013-09-25 08:20:28 +0200

    added examples for modules

---
+++ src/pester-examples/Invoke-NUnit.psm1
@@ -0,0 +1,29 @@
+﻿
+function Invoke-NUnit {
+	$arguments = @()
+	
+	$arguments += "/labels"
+	$arguments += "/out=TestResult.txt"
+	$arguments += "/xml=TestResult.xml"
+	
+	$exe = $(Get-NUnitPackage).Exe
+	
+	Invoke-Executable $exe $arguments
+}
+
+function Invoke-Executable {
+	param(
+		[String]$Exe,
+		[String[]]$Parameters
+	)
+	
+	& $Exe $Parameters | Out-Null
+
+}
+
+function Get-NUnitPackage {
+	throw "I'm not implemented yet..."
+}
+
+Export-ModuleMember -Function Invoke-NUnit
+
